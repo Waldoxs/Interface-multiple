b3335650ed880f7623f78b8d103a2a04291da162
Oswaldo <santoswaldos@gmail.com>
2025-12-05 07:51:12 -0600

    Inicilizando la práctica

---
+++ src/org/osantos/poointerfaces/modelo/Cliente.java
@@ -0,0 +1,39 @@
+package org.osantos.poointerfaces.modelo;
+
+public class Cliente {
+    private Integer id;
+    private String nombre;
+    private String apellido;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
---
+++ src/org/osantos/poointerfaces/repositorio/CrudRepositorio.java
@@ -0,0 +1,4 @@
+package org.osantos.poointerfaces.repositorio;
+
+public interface CrudRepositorio {
+}
